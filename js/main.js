31402787892539c1419855e8645950e3223d85cc
ValerioTorto <valerio.tortorelli@gmail.com>
2023-11-15 01:28:56 +0100

    adjusting

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 // Chiedere le informazioni di base all'utente
-//let kmdapercorrere = prompt("Inserisci i km che vuoi percorrere.")
-//if (isNaN(kmdapercorrere)) {
-    //alert("Non hai inserito un numero!");
-//}
-//let eta = prompt("Inserisci la tua età.")
-//if (isNaN(eta)) {
-    //alert("Non hai inserito un numero!");
-//}
+if (isNaN(kmdapercorrere)) {
+    ("Non hai inserito un numero!");
+}
+if (isNaN(eta)) {
+    alert("Non hai inserito un numero!");
+}
 
 
 // Elaborazione delle statistiche
@@ -31,8 +29,5 @@ let prezzofinale = prezzokm.toFixed(2);
 
 
 
-//document.getElementById("form_data_input").classList.add("d-none");
-// E faccio comparire la card con la password generata
-//document.getElementById("form_data_input").classList.add("d-none");
+
 }
-//document.getElementById("prezzofinale").innerHTML = prezzofinale;
